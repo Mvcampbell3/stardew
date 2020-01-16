ecd164f495400b72847a1246c623c6be1d89e223
Mvcampbell3 <mvcampbell3@gmail.com>
2020-01-16 17:31:37 -0500

    completed villager seed with non-marriable villagers

--- client/src/app/models/npcSeed.ts
+++ client/src/app/models/npcSeed.ts
@@ -24,6 +24,48 @@ const Maru = new NPC('Maru', ['Battery Pack', 'Cauliflower', 'Cheese Cauliflower
 
 const Penny = new NPC('Penny', ['Diamond', 'Emerald', 'Melon', 'Poppy', 'Poppyseed Muffin', 'Red Plate', 'Roots Platter', 'Sandfish', 'Tom Kha Soup'], 'https://stardewvalleywiki.com/mediawiki/images/a/ab/Penny.png', true);
 
+const Caroline = new NPC('Caroline', ['Fish Taco', 'Green Tea', 'Summer Spangle'], 'https://stardewvalleywiki.com/mediawiki/images/8/87/Caroline.png', false);
+
+const Clint = new NPC('Clint', ['Amethyst', 'Aquamarine', 'Artichoke Dip', 'Emerald', 'Fiddlehead Risotti', 'Gold Bar', 'Iridium Bar', 'Jade', 'Omni Geode', 'Ruby', 'Topaz'], 'https://stardewvalleywiki.com/mediawiki/images/3/31/Clint.png', false);
+
+const Demetrius = new NPC('Demetrius', ['Bean Pot', 'Ice Cream', 'Rice Pudding', 'Strawberry'], 'https://stardewvalleywiki.com/mediawiki/images/f/f9/Demetrius.png', false);
+
+const Dwarf = new NPC('Dwarf', ['Amethyst', 'Aquamarine', 'Emerald', 'Jade', 'Omni Geode', 'Ruby', 'Topaz'], 'https://stardewvalleywiki.com/mediawiki/images/e/ed/Dwarf.png', false);
+
+const Evelyn = new NPC('Evelyn', ['Beet', 'Chocoalate Cake', 'Diamond', 'Fairy Rose', 'Stuffing', 'Tulip'], 'https://stardewvalleywiki.com/mediawiki/images/8/8e/Evelyn.png', false);
+
+const George = new NPC('George', ['Fried Mushroom', 'Leek'], 'https://stardewvalleywiki.com/mediawiki/images/7/78/George.png', false);
+
+const Gus = new NPC('Gus', ['Diamond', 'Escargot', 'Fish Taco', 'Orange'], 'https://stardewvalleywiki.com/mediawiki/images/5/52/Gus.png', false);
+
+const Jas = new NPC('Jas', ['Fairy Rose', 'Pink Cake', 'Plum Pudding'], 'https://stardewvalleywiki.com/mediawiki/images/5/55/Jas.png', false);
+
+const Jodi = new NPC('Jodi', ['Chocolate Cake', 'Crispy Bass', 'Diamond', 'Eggplant Parmesan', 'Fried Eel', 'Pancakes', 'Rhubard Pie', 'Vegetable Medley'], 'https://stardewvalleywiki.com/mediawiki/images/4/41/Jodi.png', false);
+
+const Kent = new NPC('Kent', ['Fiddlehead Risotto', 'Roasted Hazelnuts'], 'https://stardewvalleywiki.com/mediawiki/images/9/99/Kent.png', false);
+
+const Krobus = new NPC('Krobus', ['Diamond', 'Iridium Bar', 'Pumpkin', 'Void Egg', 'Void Mayonnaise', 'Wild Horseradish'], 'https://stardewvalleywiki.com/mediawiki/images/7/71/Krobus.png', false);
+
+const Lewis = new NPC('Lewis', ['Autumn\'s Bounty', 'Glazed Yam', 'Green Tea', 'Hot Pepper', 'Vegetable Medley'], 'https://stardewvalleywiki.com/mediawiki/images/2/2b/Lewis.png', false);
+
+const Linus = new NPC('Linus', ['Blueberry Tart', 'Cactus Fruit', 'Coconut', 'Dish o\' The Sea', 'Yam'], 'https://stardewvalleywiki.com/mediawiki/images/3/31/Linus.png', false);
+
+const Marnie = new NPC('Marnie', ['Diamond', 'Farmer\'s Lunch', 'Pink Cake', 'Pumpkin Pie'], 'https://stardewvalleywiki.com/mediawiki/images/5/52/Marnie.png', false);
+
+const Pam = new NPC('Pam', ['Beer', 'Cactus Fruit', 'Glazed Yams', 'Mead', 'Pale Ale', 'Parsnip', 'Parsnip Soup'], 'https://stardewvalleywiki.com/mediawiki/images/d/da/Pam.png', false);
+
+const Pierre = new NPC('Pierre', ['Fried Calamari'], 'https://stardewvalleywiki.com/mediawiki/images/7/7e/Pierre.png', false);
+
+const Robin = new NPC('Robin', ['Goat Cheese', 'Peach', 'Spaghetti'], 'https://stardewvalleywiki.com/mediawiki/images/1/1b/Robin.png', false);
+
+const Sandy = new NPC('Sandy', ['Crocus', 'Daffodil', 'Sweet Pea'], 'https://stardewvalleywiki.com/mediawiki/images/4/4e/Sandy.png', false);
+
+const Vincent = new NPC('Vincent', ['Grape', 'Cranberry Candy', 'Pink Cake', 'Snail'], 'https://stardewvalleywiki.com/mediawiki/images/f/f1/Vincent.png', false);
+
+const Willy = new NPC('Willy', ['Catfish', 'Diamond', 'Iridium Bar', 'Mead', 'Octopus', 'Pumpkin', 'Sea Cucumber', 'Sturgeon'], 'https://stardewvalleywiki.com/mediawiki/images/8/82/Willy.png', false);
+
+const Wizard = new NPC('Wizard', ['Purple Mushroom', 'Solar Essence', 'Super Cucumber', 'Void Essence'], 'https://stardewvalleywiki.com/mediawiki/images/c/c7/Wizard.png', false);
+
 export default [
   Alex,
   Elliot,
@@ -36,5 +78,26 @@ export default [
   Haley,
   Leah,
   Maru,
-  Penny
+  Penny,
+  Caroline,
+  Clint,
+  Demetrius,
+  Dwarf,
+  Evelyn,
+  George,
+  Gus,
+  Jas,
+  Jodi,
+  Kent,
+  Krobus,
+  Lewis,
+  Linus,
+  Marnie,
+  Pam,
+  Pierre,
+  Robin,
+  Sandy,
+  Vincent,
+  Willy,
+  Wizard
 ]
