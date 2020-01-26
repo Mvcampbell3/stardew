ffef08d1c0608cfd0b3e223bebfc70ba1bb28725
Mvcampbell3 <mvcampbell3@gmail.com>
2020-01-25 20:40:58 -0500

    Revert "messing with material"
    This reverts commit 0861834fbb5a6cf1deb4caca49ce40541e76d833.

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -5,11 +5,10 @@ import { VillagersComponent } from './villagers/villagers.component';
 import { MatchComponent } from './match/match.component';
 import { GuideComponent } from './guide/guide.component';
 import { GiftsComponent } from './gifts/gifts.component';
-import { TestmatComponent } from './testmat/testmat.component';
 
 
 const routes: Routes = [
-  { path: '', component: TestmatComponent },
+  { path: '', component: LandingComponent },
   { path: 'villagers', component: VillagersComponent },
   { path: 'gifts', component: GiftsComponent },
   { path: 'match', component: MatchComponent },
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { MatSliderModule } from '@angular/material/slider';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,16 +13,6 @@ import { PersonComponent } from './components/person/person.component';
 import { ProfileComponent } from './profile/profile.component';
 import { GuideComponent } from './guide/guide.component';
 import { GiftsComponent } from './gifts/gifts.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { TestmatComponent } from './testmat/testmat.component';
-import { NavTestComponent } from './nav-test/nav-test.component';
-import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
-import { MatGridListModule } from '@angular/material/grid-list';
 
 @NgModule({
   declarations: [
@@ -35,24 +24,13 @@ import { MatGridListModule } from '@angular/material/grid-list';
     PersonComponent,
     ProfileComponent,
     GuideComponent,
-    GiftsComponent,
-    TestmatComponent,
-    NavTestComponent
+    GiftsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule,
-    BrowserAnimationsModule,
-    MatSliderModule,
-    LayoutModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatListModule,
-    MatGridListModule
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
